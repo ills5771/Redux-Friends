c5aa5bf4157cc5c229f3410e8a6d8b9987ee577a
ills5771 <ilyayelizarov1@gmail.com>
2019-03-07 12:42:23 -0800

    converted Login to a class component so it can take in state of user and pass

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -2,38 +2,44 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
-export default function Login() {
-  return (
-    <form
-      style={{
-        margin: "20% 25%",
-        width: "400px",
-        height: "400px",
-        padding: "5%",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-evenly",
-        border: "1px solid lightgray"
-      }}
-    >
-      <img
-        style={{ width: "300px", height: "150px" }}
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPW8QPtJLvt4eby6wb4J-5X6HGaIR6yBsHh2S3A3M2XzAAcz2J"
-        alt="friends-logo"
-      />
-      <TextField
-        id="standard-with-placeholder"
-        label="username"
-        placeholder="username"
-        margin="normal"
-      />
-      <TextField
-        id="standard-with-placeholder"
-        label="password"
-        placeholder="password"
-        margin="normal"
-      />
-      <Button variant="contained">Login</Button>
-    </form>
-  );
+class Login extends React.Component {
+  state = {
+    username: "",
+    password: ""
+  };
+  render() {
+    return (
+      <form
+        style={{
+          margin: "20% 25%",
+          width: "400px",
+          height: "400px",
+          padding: "5%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+          border: "1px solid lightgray"
+        }}
+      >
+        <img
+          style={{ width: "300px", height: "150px" }}
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPW8QPtJLvt4eby6wb4J-5X6HGaIR6yBsHh2S3A3M2XzAAcz2J"
+          alt="friends-logo"
+        />
+        <TextField
+          id="standard-with-placeholder"
+          label="username"
+          placeholder="username"
+          margin="normal"
+        />
+        <TextField
+          id="standard-with-placeholder"
+          label="password"
+          placeholder="password"
+          margin="normal"
+        />
+        <Button variant="contained">Login</Button>
+      </form>
+    );
+  }
 }
